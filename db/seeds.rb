13d91b3a80db660b639e77c963f186be10f10b15
marycriv <maryecrivelli@gmail.com>
2019-05-28 11:59:43 -0400

    Added seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+
+video1 = Video.create(:name=>"Me at the zoo")
+video2 = Video.create(:name=>"Koko the gorilla")
+video3 = Video.create(:name=>"RIP harambe")
+video4 = Video.create(:name=>"Zoboomafoo theme song")
+video5 = Video.create(:name=>"Zoobooks commercial")
+
+viewer1 = Viewer.create(:name=>"BillyBob")
+viewer2 = Viewer.create(:name=>"SallieMae")
+viewer3 = Viewer.create(:name=>"JoeyJoe")
+viewer4 = Viewer.create(:name=>"Buddy Holly")
+viewer5 = Viewer.create(:name=>"Mary Tyler Moore")
+viewer6 = Viewer.create(:name=>"Steve Irwin")
+
+view1 = 
+view2 =
+view3 =
